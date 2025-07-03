f89704cd093ca25cad2a9f0368daea3350b2d960
mane <53539004+tommhe14@users.noreply.github.com>
2025-07-03 18:02:45 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="cs2api",
-    version="0.1.0",  
+    version="0.1.1",  
     description="Python API wrapper for Counter-Strike 2 professional match data via BO3.gg",
     long_description=README,
     long_description_content_type="text/markdown",
